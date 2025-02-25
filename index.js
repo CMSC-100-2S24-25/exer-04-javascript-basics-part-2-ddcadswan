523da2879e01b32ac9c313117bf2ccc47ead512c
unknown <ddcadsawan@up.edu.ph>
2025-02-26 00:31:36 +0800

    chore: Added a generateUniqueID function in index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //NAME: Dalton Ken D. Cadsawan
+
+
+// Imports
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const validator = require("validator");
+
+// FUNCTIONS
+function generateUniqueID(firstName, lastName){
+    if (typeof firstName !== 'string' || typeof lastName !== 'string'){
+        console.log("ERROR: Inputs for name must be a string.");
+    }
+
+    const alphaNum = uuidv4().slice(0,8); //take an alphanumeric string of length 8 
+    return "${firstName[0].toLowerCase()}${lastName.toLowerCase()}${alphaNum}";
+} // end of generateUniqueID
+
+module.exports = { generateUniqueID };
